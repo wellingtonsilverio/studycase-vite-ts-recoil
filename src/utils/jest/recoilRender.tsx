e74b3e505e32f9a6bc3ba1c4e6f51d4ef919f345
Wellington Silverio <wellington.alves.silverio@gmail.com>
2023-11-27 10:56:50 -0300

    refactor: formater

--- src/utils/jest/recoilRender.tsx
+++ src/utils/jest/recoilRender.tsx
@@ -2,11 +2,7 @@ import { render } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 
 const recoilRender = (component: JSX.Element) => {
-  return render(
-    <RecoilRoot>
-      {component}
-    </RecoilRoot>
-  )
-}
+  return render(<RecoilRoot>{component}</RecoilRoot>);
+};
 
 export default recoilRender;
