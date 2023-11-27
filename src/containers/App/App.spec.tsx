6c12ae4d3ada333baa30bea0d7ac923319343bb2
Wellington Silverio <wellington.alves.silverio@gmail.com>
2023-11-27 10:54:38 -0300

    test: create wrapper to recoil tests

--- src/containers/App/App.spec.tsx
+++ src/containers/App/App.spec.tsx
@@ -1,11 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
 
 import App from '.';
 
+const renderWithRecoil = (component: JSX.Element) => {
+  return render(
+    <RecoilRoot>
+      {component}
+    </RecoilRoot>
+  )
+}
+
 describe('App Container', () => {
   it('should display initial screen', () => {
-    render(<App />);
+    renderWithRecoil(<App />);
 
-    expect(screen.getByRole('heading', { name: /vite \+ react/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /vite \+ react \+ recoil/i })).toBeInTheDocument();
   });
 });
