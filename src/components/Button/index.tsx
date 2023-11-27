b253c23d2e86234da31944319aa5edd695c760e9
Wellington Silverio <wellington.alves.silverio@gmail.com>
2023-11-27 10:41:47 -0300

    refactor: update lint warns

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface Props {
     children?: React.ReactNode;
     onClick?: () => void;
-};
+}
 
 const Button: React.FC<Props> = ({onClick, children}) => {
     return (
