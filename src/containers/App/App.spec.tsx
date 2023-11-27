b4807976b7bdfd943cd8373d95900ffbcfd0a465
Wellington Silverio <wellington.alves.silverio@gmail.com>
2023-11-27 08:40:25 -0300

    refactor: folders with structure containers+components

--- src/containers/App/App.spec.tsx
+++ src/containers/App/App.spec.tsx
@@ -3,13 +3,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe("App", () => {
-    it('should work', () => {
-        expect(1).toBe(1)
-    });
-
     it('should display initial screen', () => {
         render(<App />);
 
-        screen.debug();
+        expect(screen.getByRole('heading', { name: /vite \+ react/i })).toBeInTheDocument()
     });
 });
